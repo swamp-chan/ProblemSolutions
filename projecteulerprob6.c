200ae5f83b0f5b451180560927ae364c6973a1c4
swampychem <sambeedshrestha123@gmail.com>
2025-01-15 23:54:12 +0545

    Update projecteulerprob6.c

--- projecteulerprob6.c
+++ projecteulerprob6.c
@@ -7,14 +7,10 @@ typedef long long int ll;
 
 int main(){
 	ll sum1 = 0, sum2 = 0;
-	for(int i = 1; i<101; i++){
-		sum1 += i*i;
-	}
-	for(int j = 1; j<101; j++){
-		sum2 += j;
-	}
-	sum2 *= sum2;
-	printf("%lld",sum2-sum1);
+	int n = 100;
+	sum1 = n*(n+1)/2;
+	sum2 = n*(n+1)*(2*n+1)/6;
+	printf("%lld",sum1*sum1-sum2);
 	getch();
 	return 0;
 }
